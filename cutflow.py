30f6567665ad2e5631d8056f2caab063b62ffe07
Chris Milke <chrisdmilke@gmail.com>
2020-07-14 09:49:23 -0500

    Better Cutflow diagram

--- cutflow.py
+++ cutflow.py
@@ -2,43 +2,71 @@
 
 import sys
 import uproot
+from matplotlib import pyplot as plt
+
+_stages_to_print = [
+    'Trigger',
+    '6 jets, pT > 30 GeV',
+    'VBF Pair',
+    'VBF dEta',
+    'VBF mjj',
+    #'VBF BDT',
+    '4 good jets (w/o IS), >= 2 tagged',
+    'Multi Tagged',
+    'Valid',
+    'VBF pTsum',
+    'pT(h)s',
+    'dEta_hh',
+    'Main Xwt'
+]
+
+_cvv_values = [0,0.5,1,1.5,2,4]
+#_cvv_values = [0,0.5,1,1.5,2]
+#_cvv_values = [1]
 
 
-
-def get_vals(outputname, hist_name, stages_to_print):
+def get_cutflow_values(outputname, hist_name, stages_to_print):
     directory = uproot.open(outputname)
     cutflow_hist = directory[hist_name]
     labeled_values = { k:v for k,v in zip(cutflow_hist.xlabels, cutflow_hist.values) }
-    recorded_vals = [ labeled_values[key] for key in stages_to_print ]
-    for val in recorded_vals: print(f'{val:.4f} ', end=' ')
-    print()
-        
+    recorded_values = [ labeled_values[key] for key in stages_to_print ]
+    return recorded_values
 
 
 def main():
-    base_names = { val:'VBF-HH-bbbb_cvv'+str(val).replace('.','p') for val in [ 0, 0.5, 1, 1.5, 2, 4 ] }
+    base_names = {}
     base_names['ggF'] = 'ggF-HH-bbbb'
+    base_names.update( { val:'VBF-HH-bbbb_cvv'+str(val).replace('.','p') for val in _cvv_values } )
 
-    #print(' 6Jet | Pair | VSel | Deta | mjj  |  HH  |')
+    plot_data = {'x':[],'weights':[],'label':[],'histtype':'step','linewidth':2}
+    x_values = list(range(len(_stages_to_print)))
+    cutflow_type = 'FourTagCutflow'
     for cvv, base in base_names.items():
-        print(cvv)
-        for mode in ['dump', 'cut']:
-            stages_to_print = [
-                #'6 jets, pT > 30 GeV',
-                'VBF Pair',
-                'VBF dEta',
-                'VBF mjj',
-                'VBF BDT',
-                '4 good jets (w/o IS), >= 2 tagged',
-                'Multi Tagged',
-                'Valid',
-                'VBF pTsum',
-                'pT(h)s',
-                'dEta_hh'
-            ]
-            outputname = '../output/'+sys.argv[1]+'/output_MC16d_'+base+'_'+mode+'.root'
-            get_vals(outputname, 'FourTagCutflow', stages_to_print)
-        print('-----------------------------------------')
+        outputname = '../nano_ntuples/V5/output_MC16d_'+base+'.root'
+        cutflow_values = get_cutflow_values(outputname,cutflow_type, _stages_to_print)
+        plot_data['x'].append(x_values)
+        plot_data['weights'].append(cutflow_values)
+        plot_data['label'].append( cvv if cvv == 'ggF' else '$C_{2V}$='+str(cvv) )
+
+    fig,ax = plt.subplots()
+    counts, bins, hist = ax.hist(**plot_data, bins=len(x_values), range=(0,len(x_values)) )
+    plt.grid()
+    plt.legend(fontsize=5)
+    x_tick_labels = _stages_to_print.copy()
+    x_tick_labels[ x_tick_labels.index('4 good jets (w/o IS), >= 2 tagged') ] = '4 w/o IS'
+    x_tick_labels[ x_tick_labels.index('Multi Tagged') ] = '4Tag'
+    x_tick_labels[ x_tick_labels.index('6 jets, pT > 30 GeV') ] = '6 Jets'
+    plt.xticks(ticks=bins, labels=x_tick_labels, rotation=45, fontsize='xx-small')
+    plt.ylabel('Weight Sum (Events / $fb^{-1}$ of data)')
+    plt.yscale('log')
+    plt.title(cutflow_type)
+    plt.setp( ax.xaxis.get_majorticklabels(), ha='left')
+    #plt.tight_layout()
+    plt.savefig('figures/cutflow.png', dpi=500)
+
+
+
+
 
 
 
--- final_roc.py
+++ final_roc.py
@@ -11,6 +11,8 @@ def get_final_sig(outputname, hist_name, stage_to_print):
     directory = uproot.open(outputname)
     cutflow_hist = directory[hist_name]
     labeled_values = { k:v for k,v in zip(cutflow_hist.xlabels, cutflow_hist.values) }
+    #for key, val in labeled_values.items(): print(key, val)
+    #exit()
     return labeled_values[stage_to_print]
         
 
@@ -20,13 +22,17 @@ def main():
     base_names = { val:'VBF-HH-bbbb_cvv'+str(val).replace('.','p') for val in [ 0, 0.5, 1, 1.5, 2 ] }
     base_names['ggF'] = 'ggF-HH-bbbb'
 
-    cutvalues = numpy.arange(-0.2, 0.21, 0.01)
+    cutvalues = numpy.arange(-0.25, 0.5, 0.01)
     cutstrlist = []
     for cutval in cutvalues:
         cutstrlist.append(re.sub('0(?=[.])', '', f'{cutval:.02f}'))
-    cutstrlist[ cutstrlist.index('-.20') ] = '-0.2'
+    #cutstrlist[ cutstrlist.index('-.20') ] = '-0.2'
     cutstrlist[ cutstrlist.index('.00') ] = '0'
     cutlist = list( zip(cutvalues, cutstrlist) )
+
+    main_cut = 'Main Xwt'
+    signal_region = 'FourTagCutflow'
+    #signal_region = 'TwoTagCutflow'
     
 
 
@@ -37,13 +43,15 @@ def main():
         data[cvv] = ([],[])
         print(cvv)
         for cutval, cutstr in cutlist:
-            ntuple = ntupledir+'scan00/output_MC16d_'+base+'_scanVBFcut_'+cutstr+'.root'
-            significance = get_final_sig(ntuple, 'FourTagCutflow', 'dEta_hh')
+            ntuple = ntupledir+'scan01/output_MC16d_'+base+'_scanVBFcut_'+cutstr+'.root'
+            significance = get_final_sig(ntuple, signal_region, main_cut)
             data[cvv][0].append(cutval)
             data[cvv][1].append(significance)
         base_ntuple = ntupledir+'V9/output_MC16d_'+base+'_dump.root'
-        base_sig[cvv] = get_final_sig(base_ntuple, 'FourTagCutflow', 'dEta_hh')
+        base_sig[cvv] = get_final_sig(base_ntuple, signal_region, main_cut)
     print(base_sig)
+    base_points = { cvv:( vbf_sig, base_sig['ggF'] ) for cvv, vbf_sig in base_sig.items() if cvv != 'ggF' }
+    print(base_points)
 
     #for key, (cuts,sigs) in data.items():
     #    print(key)
@@ -51,30 +59,50 @@ def main():
     #    print()
 
     for key, (cuts,sigs) in data.items():
-        plt.plot(cuts, sigs, label=key)
+        title = key if key == 'ggF' else '$C_{2V}$='+str(key)
+        plt.plot(cuts, sigs, label=title)
     plt.legend()
     plt.grid(True)
-    plt.xlabel('BDT Cut Value')
-    plt.ylabel(r'Significance of $\Delta \eta_{HH}$')
+    plt.xlabel('VBF-BDT Cut Value')
+    plt.ylabel(r'Event Weight Sum (Events / $fb^{-1}$ of data)')
     #plt.ylim(0,0.33)
     plt.yscale('log')
-    plt.title('Cut VS Significance')
-    plt.savefig('figures/significance_comparison.png')
+    plt.title('Cut VS \"'+main_cut+'\" Event Weight Sum ('+signal_region+')')
+    plt.savefig('figures/final_weight_comparison_'+signal_region+'.png')
     plt.close()
 
     signal_dict = { key:val[1] for key,val in data.items() if key != 'ggF' }
     background= data['ggF'][1]
 
-    for key, signal in signal_dict.items():
-        plt.plot(signal, background, label=key)
+    for key, signal in signal_dict.items(): 
+        title = key if key == 'ggF' else '$C_{2V}$='+str(key)
+        plt.plot(signal, background, label=title)
+    plt.axhline(base_sig['ggF'], color='black')
+
     plt.legend()
     plt.grid(True)
-    plt.xlabel('VBF Signficance')
-    plt.ylabel('ggF Signficance')
+    plt.xlabel('VBF Weight Sum (Events / $fb^{-1}$ of data)')
+    plt.ylabel('ggF Weight Sum (Events / $fb^{-1}$ of data)')
+    plt.xlim(1e-3,0.33)
     plt.xscale('log')
-    plt.title('ggF VS VBF Significance')
-    plt.savefig('figures/final_roc.png')
+    plt.ylim(1e-3,0.33)
+    plt.yscale('log')
+    plt.title('ggF VS VBF \"'+main_cut+'\" Event Weight Sum ('+signal_region+')')
+
+    sig_colors = {
+        0:   'blue',
+        0.5: 'orange',
+        1:   'green',
+        1.5: 'red',
+        2:   'purple'
+    }
+    for cvv, (x,y) in base_points.items():
+        plt.plot(x,y, label=cvv, color=sig_colors[cvv], marker='x')
+
+
+    plt.savefig('figures/final_roc_'+signal_region+'.png')
     plt.close()
+
 
 
 
